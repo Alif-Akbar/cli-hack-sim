7ea3f34d16eb8eb24fdd6fe14e4f8d093ddfe782
irfankurniawan25 <irfankurniawan25pku@gmail.com>
2026-05-17 21:14:37 +0700

    tambah menu

---
+++ src/main_menu.py
@@ -0,0 +1,26 @@
+print('=' * 31)
+print('=== HACKER NETWORK SYSTEM ===')
+print('=' * 31)
+while True:
+    pilih = input('''
+1. Login
+2. Tambah Server
+3. Tampilkan Server
+4. Hubungkan Server
+5. Kirim Packet
+0. Exit
+Pilih Menu: ''')
+
+    if pilih == '1':
+        pass
+    elif pilih == '2':
+        pass
+    elif pilih == '3':
+        pass
+    elif pilih == '4':
+        pass
+    elif pilih == '5':
+        pass
+    elif pilih == '0':
+        print('Program Selesai!')
+        exit()
