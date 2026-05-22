99f06d29fea8b9f16734c9ac3b24a13eb19d3480
irfankurniawan25 <irfankurniawan25pku@gmail.com>
2026-05-22 14:06:03 +0700

    update file handler

--- src/filehandler.py
+++ src/filehandler.py
@@ -1,74 +1,100 @@
 from server import *
 from login import *
 
-# fungsi untuk menyimpan data server ke dalam file txt
-def save_server():
-    with open('data/server.txt', 'w') as f:
-        for i in server_list:
-            data = f'{i.nama},{i.ip},{i.status},{i.traffic}\n'
-            f.write(data)
+class FileHandler:
+    def __init__(self):
+        pass
+    
+    # fungsi untuk menyimpan data server ke dalam file txt
+    def save_server(self):
+        with open('data/server.txt', 'w') as f:
+            for i in server_list:
+                data = f'{i.nama},{i.ip},{i.status},{str(i.traffic)}\n'
+                f.write(data)
 
-# fungsi untuk mengambil data dari file txt
-def load_server():
-    try:
-        server_list.clear()
+    # fungsi untuk mengambil data dari file txt
+    def load_server(self):
+        try:
+            server_list.clear()
+            
+            with open('data/server.txt', 'r') as f:
+                for baris in f:
+                    data = baris.strip().split(',') # ini untuk mengubah text menjadi sebuah data dalam list
+                    nama = data[0]
+                    ip = data[1]
+                    status = data[2]
+                    traffic = int(data[3])
+                    
+                    obj = Server(nama, ip, status, traffic) # data kembali menjadi objek 
+                    server_list.append(obj)
+        except:
+            print('File Tidak Ditemukan')
+
+    # fungsi untuk menyimpan data akun ke dalam file txt
+    def save_akun(self):
+        with open('data/akun.txt', 'w') as f:
+            for username in akun:
+                password = akun[username]
+                f.write(f'{username},{password}\n')
+
+    # fungsi untuk mengambil data dari file txt
+    def load_akun(self):
+        try:
+            with open('data/akun.txt', 'r') as f:
+                for baris in f:
+                    data = baris.strip().split(',')
+                    username = data[0]
+                    password = data[1]
+                    akun[username] = password
+        except:
+            print('File Tidak Ditemukan')
+
+    # fungsi untuk menyimpan log stack
+    def save_log(self, log_stack):
+        with open('data/log.txt', 'w') as f:
+            for log in log_stack:
+                f.write(log + '\n')
+
+    # fungsi untuk mengambil data dari file log.txt
+    def load_log(self, log_stack):
+        try:
+            log_stack.clear()
+            
+            with open('data/log.txt', 'r') as f:
+                for baris in f:
+                    log_stack.append(baris.strip())
+        except:
+            print('File Tidak Ditemukan')
+
+    # fungsi untuk menyimpan data packet
+    def save_packet(self, packet_queue):
+        with open('data/packet.txt', 'w') as f:
+            for packet in packet_queue:
+                f.write(packet + '\n')
+
+    # fungsi untuk mengambil data dari file packet.txt
+    def load_packet(self, packet_queue):
+        try:
+            packet_queue.clear()
+            
+            with open('data/packet.txt', 'r') as f:
+                for baris in f:
+                    packet_queue.append(baris.strip())
+        except:
+            print('File Tidak Ditemukan')
+
+    def save_all(self):
+        self.save_server()
+        self.save_akun()
+        self.save_log()
+        self.save_packet()
         
-        with open('data/server.txt', 'r') as f:
-            for baris in f:
-                data = baris.strip().split(',') # ini untuk mengubah text menjadi sebuah data dalam list
-                nama = data[0]
-                ip = data[1]
-                status =[2]
-                traffic = data[3]
-                
-                obj = Server(nama, ip, status, traffic) # data kembali menjadi objek 
-                server_list.append(obj)
-    except:
-        print('File Tidak Ditemukan')
+        print('[ALL DATA SAVED]')
 
-# fungsi untuk menyimpan data akun ke dalam file txt
-def save_akun():
-    with open('data/akun.txt', 'w') as f:
-        for username in akun:
-            password = akun[username]
-            f.write(f'{username},{password}\n')
-
-# fungsi untuk mengambil data dari file txt
-def load_akun():
-    try:
-        with open('data/akun.txt', 'r') as f:
-            for baris in f:
-                data = baris.strip().split(',')
-                username = data[0]
-                password = data[1]
-                akun[username] = password
-    except:
-        print('File Tidak Ditemukan')
-
-def save_log():
-    pass
-
-def load_log():
-    pass
-
-def save_packet():
-    pass
-
-def load_packet():
-    pass
-
-def save_all():
-    save_server()
-    save_akun()
-    save_log()
-    save_packet()
-    
-    print('[ALL DATA SAVED]')
-
-def load_all():
-    load_server()
-    load_akun()
-    load_log()
-    load_packet()
-    
-    print('[ALL DATA LOADED]')
+    def load_all(self):
+        self.load_server()
+        self.load_akun()
+        self.load_log()
+        self.load_packet()
+        
+        print('[ALL DATA LOADED]')
