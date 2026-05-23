d12bec58e315b77de61c3e24a78818f535f424d0
irfankurniawan25 <irfankurniawanpku25@gmail.com>
2026-05-23 12:16:53 +0700

    Update traversal_folder.py

--- DSA/traversal_folder.py
+++ DSA/traversal_folder.py
@@ -1,19 +1,24 @@
 class BST:
     def __init__(self):
         pass
-    
+
+    # root - left - right
     def preorder(self, node):
         if node is not None:
             print(node.data, end=' ')
             self.preorder(node.left)
             self.preorder(node.right)
-    
+
+    # left - root - right 
     def inorder(self, node):
-        self.in_order(node.left)
-        print(node.data, end=' ')
-        self.in_order(node.right)
-    
+        if node is not None:
+            self.in_order(node.left)
+            print(node.data, end=' ')
+            self.in_order(node.right)
+
+    # left - right - root
     def postorder(self, node):
-        self.postorder(node.left)
-        self.postorder(node.right)
-        print(node.data, end=' ')
+        if node is not None:
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.data, end=' ')
