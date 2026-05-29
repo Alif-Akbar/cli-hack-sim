e9563762419ad798f6675c1fea6cf9026a68e7c5
irfankurniawan25 <irfankurniawan25pku@gmail.com>
2026-05-29 22:18:39 +0700

    update dan menambahkan save & load json

--- src/filehandler.py
+++ src/filehandler.py
@@ -7,16 +7,17 @@ class FileHandler:
     
     # fungsi untuk menyimpan data server ke dalam file txt
     def save_server(self):
+        fs = FungsiServer()
+        
         with open('data/server.txt', 'w') as f:
-            for i in server_list:
+            for i in fs.server_list:
                 data = f'{i.nama}|{i.id}|{i.ip}|{i.status}\n'
                 f.write(data)
 
     # fungsi untuk mengambil data dari file txt
     def load_server(self):
+        server_list = []
         try:
-            server_list.clear()
-            
             with open('data/server.txt', 'r') as f:
                 for baris in f:
                     data = baris.strip().split('|') # ini untuk mengubah text menjadi sebuah data dalam list
@@ -25,25 +26,28 @@ class FileHandler:
                     ip = data[2]
                     status = data[3]
                     
-                    obj = Server(nama, id, ip, status) # data kembali menjadi objek 
+                    obj = ServerNode(nama, id, ip, status) # data kembali menjadi objek 
                     server_list.append(obj)
+            return server_list
         except:
             print('File Tidak Ditemukan')
+            return
 
     # fungsi untuk menyimpan data akun ke dalam file json
     def save_akun(self):
+        l = Login()
         with open('data/dalam-json/akun.json', 'w') as f:
-            json.dump(servers, f, indent=4)
+            json.dump(l.servers, f, indent=4)
 
     # fungsi untuk mengambil data dari file json
     def load_akun(self):
+        l = Login()
         try:
-            servers.clear()
             with open('data/dalam-json/akun.json', 'r') as f:
-                data = json.load(f)
-                servers.update(data)
+                return json.load(f)
         except:
             print('File Tidak Ditemukan')
+            return
 
     # fungsi untuk menyimpan log stack
     def save_log(self, log_stack):
@@ -52,13 +56,13 @@ class FileHandler:
                 f.write(log + '\n')
 
     # fungsi untuk mengambil data dari file log.txt
-    def load_log(self, log_stack):
+    def load_log(self):
+        log_stack = []
         try:
-            log_stack.clear()
-            
             with open('data/log.txt', 'r') as f:
                 for baris in f:
                     log_stack.append(baris.strip())
+            return log_stack
         except:
             print('File Tidak Ditemukan')
 
@@ -69,15 +73,25 @@ class FileHandler:
                 f.write(packet + '\n')
 
     # fungsi untuk mengambil data dari file packet.txt
-    def load_packet(self, packet_queue):
+    def load_packet(self):
+        packet_queue = []
         try:
-            packet_queue.clear()
-            
             with open('data/packet.txt', 'r') as f:
                 for baris in f:
                     packet_queue.append(baris.strip())
+            return packet_queue
         except:
             print('File Tidak Ditemukan')
+    
+    # fungsi untuk menyimpan file json
+    def save_json(self, path: str, data):
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+    
+    # fungsi untuk memuat file json
+    def load_json(self, path: str):
+        with open(path, 'r') as f:
+            return json.load(f)
 
     def save_all(self):
         self.save_server()
@@ -87,15 +101,6 @@ class FileHandler:
         
         print('[ALL DATA SAVED]')
 
-    def load_all(self):
-        self.load_server()
-        self.load_akun()
-        # self.load_log()
-        # self.load_packet()
-        
-        print('[ALL DATA LOADED]')
-
 if __name__ == '__main__':
     fh = FileHandler()
     fh.save_all()
-    fh.load_all()
